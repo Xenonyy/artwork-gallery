bbc150f7162edc2dbdb8f3abbca669a5e416b7f1
Xenon <gonda.armand@gmail.com>
2022-06-16 13:17:11 +0200

    feat: add default layout for pages

--- src/components/layouts/defaultLayout.tsx
+++ src/components/layouts/defaultLayout.tsx
@@ -1,5 +1,16 @@
 import type { FC } from 'react';
 
+import { Footer } from 'components/common/footer/footer';
+import { Header } from 'components/common/header/header';
+
 export const DefaultLayout: FC = ({ children }) => {
-  return <main>{children}</main>;
+  return (
+    <>
+      <Header />
+      <main className="flex justify-center items-start w-full h-full bg-black bg-opacity-20">
+        {children}
+      </main>
+      <Footer />
+    </>
+  );
 };
