2d2acb8c83f0c7a3a002101553cd1bf5b327b16d
Xenon <gonda.armand@gmail.com>
2022-06-17 06:10:48 +0200

    feat: add enums

--- src/enums/queryKeys.ts
+++ src/enums/queryKeys.ts
@@ -1,1 +1,5 @@
-export enum QueryKeys {}
+export enum QueryKeys {
+  ARTWORKS = 'https://api.artic.edu/api/v1/artworks',
+  ARTWORK_IMAGE = 'https://www.artic.edu/iiif/2/',
+  SPECIFIC_ARTWORK = 'https://api.artic.edu/api/v1/artworks/',
+}
