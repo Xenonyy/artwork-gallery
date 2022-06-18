8e34f2cffc14a8140b3b3e1ae15e97489631ae5c
Xenon <gonda.armand@gmail.com>
2022-06-18 21:50:25 +0200

    refactor: use types, return actual values instead of console logs

--- src/redux/reducers/artworksReducer.ts
+++ src/redux/reducers/artworksReducer.ts
@@ -1,35 +1,27 @@
 import { actionTypes } from 'redux/actions/actionTypes';
+import type { AxiosPayloadType } from 'types/axiosPayloadType';
+import type { FetchArtworkStateType } from 'types/fetchArtworkStateType';
 
-interface arraytypes {
-  id: number;
-  alt_titles: string | null;
-  title: string;
-  image_id: string;
-}
-
-export const artworksReducer = (
-  state = {},
-  action: {
-    type: string;
-    payload: { data: Record<string, arraytypes[]> };
-    error: unknown;
-  },
-) => {
+export const artworksReducer = (state: FetchArtworkStateType = null, action: AxiosPayloadType) => {
   switch (action.type) {
     case actionTypes.FETCH_ARTWORKS_FULFILLED:
       return {
         ...state,
         data: action.payload.data,
+        status: action.meta.requestStatus,
+        pagination: action.payload.pagination,
       };
 
     case actionTypes.FETCH_ARTWORKS_PENDING:
-      console.log('loading');
-
-      return state;
+      return {
+        ...state,
+        status: action.meta.requestStatus,
+      };
     case actionTypes.FETCH_ARTWORKS_REJECTED:
-      console.log('rejected');
-
-      return state;
+      return {
+        ...state,
+        status: action.meta.requestStatus,
+      };
 
     default:
       return state;
--- src/redux/reducers/specificArtworkReducer.ts
+++ src/redux/reducers/specificArtworkReducer.ts
@@ -1,34 +1,31 @@
 import { actionTypes } from 'redux/actions/actionTypes';
+import type { AxiosPayloadType } from 'types/axiosPayloadType';
+import type { FetchArtworkStateType } from 'types/fetchArtworkStateType';
 
 export const specificArtworkReducer = (
-  state = {},
-  action: {
-    type: string;
-    payload: { data: Record<string, unknown[]> };
-    error: unknown;
-  },
+  state: FetchArtworkStateType = null,
+  action: AxiosPayloadType,
 ) => {
   switch (action.type) {
     case actionTypes.FETCH_ARTWORK_FULFILLED:
-      console.log('success', action);
-
       return {
         ...state,
         data: action.payload.data,
+        status: action.meta.requestStatus,
       };
 
     case actionTypes.FETCH_ARTWORK_PENDING:
-      console.log('loading');
-
-      return state;
+      return {
+        ...state,
+        status: action.meta.requestStatus,
+      };
     case actionTypes.FETCH_ARTWORK_REJECTED:
-      console.log('rejected');
-
-      return state;
+      return {
+        ...state,
+        status: action.meta.requestStatus,
+      };
 
     default:
-      console.log('default');
-
       return state;
   }
 };
