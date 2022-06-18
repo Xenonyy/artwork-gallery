cdad566fa847a51ecdfa206d0f53c237dd9f59d3
Xenon <gonda.armand@gmail.com>
2022-06-18 21:52:21 +0200

    fix: pagination interface

--- src/types/axiosPayloadType.ts
+++ src/types/axiosPayloadType.ts
@@ -2,6 +2,16 @@ import type { PayloadStatusTypes } from 'types/payloadTypes';
 
 export interface AxiosPayloadType {
   type: string;
-  payload: { data: Record<string, unknown[]>; pagination: Record<string, unknown[]> };
+  payload: {
+    data: Record<string, unknown[]>;
+    pagination: {
+      current_page: number;
+      limit: number;
+      next_url: string;
+      offset: number;
+      total: number;
+      total_pages: number;
+    };
+  };
   meta: { arg: string; requestId: string; requestStatus: PayloadStatusTypes };
 }
