e397503d46fa7c165a0ccdf5a233823197a8ddb2
Xenon <gonda.armand@gmail.com>
2022-06-19 01:51:59 +0200

    refactor: add details artwork type as well to data

--- src/types/axiosPayloadType.ts
+++ src/types/axiosPayloadType.ts
@@ -1,11 +1,12 @@
 import type { ArtworkResponseType } from 'types/artworkResponseType';
 import type { PaginationType } from 'types/paginationType';
 import type { PayloadStatusTypes } from 'types/payloadTypes';
+import type { SpecificArtworkResponseType } from 'types/specificArtworkResponseType';
 
 export interface AxiosPayloadType {
   type: string;
   payload: {
-    data: ArtworkResponseType;
+    data: ArtworkResponseType | SpecificArtworkResponseType;
     pagination: PaginationType;
   };
   meta: { arg: string; requestId: string; requestStatus: PayloadStatusTypes };
