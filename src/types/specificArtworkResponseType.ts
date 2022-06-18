7cc5066d40a1f4223408d6ee41066c2856114e6b
Xenon <gonda.armand@gmail.com>
2022-06-19 01:50:47 +0200

    feat: add artwork details type

---
+++ src/types/specificArtworkResponseType.ts
@@ -0,0 +1,11 @@
+export interface SpecificArtworkResponseType {
+  id: number;
+  alt_titles: string | null;
+  title: string;
+  image_id: string;
+  artist_title: string;
+  department_title: string;
+  thumbnail: {
+    alt_text: string;
+  };
+}
