54548d35e73e8653062119a5d5e5e1afced8e968
Xenon <gonda.armand@gmail.com>
2022-06-18 21:47:51 +0200

    fix: move footer to the bottom of the page instead of fixed pos

--- src/components/common/footer/footer.tsx
+++ src/components/common/footer/footer.tsx
@@ -8,7 +8,7 @@ const FooterComponent: FC = ({}) => {
   const intl = useIntl();
 
   return (
-    <div className="h-20 flex text-white flex-row justify-between items-center py-2 px-20 w-full bg-secondary fixed bottom-0 z-10">
+    <div className="h-20 flex text-white flex-row justify-between items-center py-2 px-20 w-full bg-secondary relative z-10">
       <p>{intl.formatMessage({ id: 'common:footer.title', defaultMessage: 'Artwork Gallery' })}</p>
       <AppVersion />
     </div>
