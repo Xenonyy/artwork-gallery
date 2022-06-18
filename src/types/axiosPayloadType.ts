1bc7b0be8a137d16c5bfee894a22437644b3ba44
Xenon <gonda.armand@gmail.com>
2022-06-18 22:07:05 +0200

    refactor: abstract types

--- src/types/axiosPayloadType.ts
+++ src/types/axiosPayloadType.ts
@@ -1,17 +1,12 @@
+import type { ArtworkResponseType } from 'types/artworkResponseType';
+import type { PaginationType } from 'types/paginationType';
 import type { PayloadStatusTypes } from 'types/payloadTypes';
 
 export interface AxiosPayloadType {
   type: string;
   payload: {
-    data: Record<string, unknown[]>;
-    pagination: {
-      current_page: number;
-      limit: number;
-      next_url: string;
-      offset: number;
-      total: number;
-      total_pages: number;
-    };
+    data: ArtworkResponseType;
+    pagination: PaginationType;
   };
   meta: { arg: string; requestId: string; requestStatus: PayloadStatusTypes };
 }
