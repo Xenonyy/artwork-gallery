d9211198aa8faa5c8a3c8bc79ad165a5614229a5
Xenon <gonda.armand@gmail.com>
2022-06-18 21:47:18 +0200

    fix: container layout

--- src/components/common/container/container.tsx
+++ src/components/common/container/container.tsx
@@ -10,7 +10,7 @@ export const Container: FC<ContainerTypes> = ({ children, alignStart, className 
   return (
     <section
       className={clsx(
-        'my-20 flex py-10 px-20 flex-col justify-start w-full h-full min-h-[80vh]',
+        'mt-20 flex py-10 px-20 flex-col justify-start min-w-screen min-h-screen',
         className,
         {
           ['items-start']: alignStart,
