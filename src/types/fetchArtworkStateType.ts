b278da54e3e22984c96a0d6f6f8d9d6fa1717df6
Xenon <gonda.armand@gmail.com>
2022-06-18 22:07:20 +0200

    feat: add pagination, fix data type

--- src/types/fetchArtworkStateType.ts
+++ src/types/fetchArtworkStateType.ts
@@ -1,6 +1,9 @@
+import type { ArtworkResponseType } from 'types/artworkResponseType';
+import type { PaginationType } from 'types/paginationType';
 import type { PayloadStatusTypes } from 'types/payloadTypes';
 
 export type FetchArtworkStateType = {
-  data: string[];
+  data: ArtworkResponseType;
   status: PayloadStatusTypes;
+  pagination: PaginationType;
 } | null;
