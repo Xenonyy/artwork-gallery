0416cdd49d4bb374df6a5781ae1a5cc3eb2cc20d
Gonda Armand <gonda.armand@arteries.hu>
2022-06-21 09:29:24 +0200

    feat: add pagination, better status handling

--- src/components/pages/gallery-list/galleryListContent.tsx
+++ src/components/pages/gallery-list/galleryListContent.tsx
@@ -1,26 +1,31 @@
-/* eslint-disable @typescript-eslint/no-unsafe-call */
-/* eslint-disable @typescript-eslint/no-unsafe-member-access */
 import { useRouter } from 'next/router';
 import type { FC } from 'react';
 import { useEffect, useState, useCallback, memo } from 'react';
+import { FormattedMessage } from 'react-intl';
 import { useDispatch, useSelector } from 'react-redux';
 
 import { ListboxMenu } from 'components/common/dropdown/listboxMenu';
+import { Pagination } from 'components/common/pagination/pagination';
 import { GalleryListItem } from 'components/pages/gallery-list/galleryListItem';
-import { messages } from 'components/pages/gallery-list/messages';
 import { fetchArtworksAction } from 'redux/actions/fetchArtworks';
 import { setArtworkIdAction } from 'redux/actions/setArtworkId';
 import type { AppDispatch, RootState } from 'redux/store';
+import type { ArtworkResponseType } from 'types/artworkResponseType';
 import { webPaths } from 'webPaths';
 
 const GalleryListContentPageComponent: FC = ({}) => {
   const router = useRouter();
   const dispatch = useDispatch<AppDispatch>();
-  const artworkData = useSelector((state: RootState) => state.artworks);
   const artworkLimitData = useSelector((state: RootState) => state.artworkLimit);
+  const artworkData = useSelector((state: RootState) => state.artworks);
+  const { pagination, data, status } = artworkData
+    ? artworkData
+    : { pagination: null, data: null, status: null };
 
   useEffect(() => {
-    void dispatch(fetchArtworksAction(artworkLimitData));
+    void dispatch(
+      fetchArtworksAction({ limit: artworkLimitData, page: String(pagination?.current_page ?? 1) }),
+    );
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [artworkLimitData]);
 
@@ -67,28 +72,65 @@ const GalleryListContentPageComponent: FC = ({}) => {
     [favoriteArtworks],
   );
 
-  return artworkData.data?.length > 0 ? (
-    <section className="w-full h-full bg-secondary bg-opacity-70 flex justify-center items-center flex-row flex-wrap px-5 py-8 rounded-3xl">
-      <ListboxMenu />
-      {artworkData.data?.map(
-        (e: { id: number; alt_titles: string | null; title: string; image_id: string }) => {
-          return (
-            <GalleryListItem
-              key={e.id}
-              alt={e.alt_titles ?? e.title}
-              favorite={favoriteArtworks.includes(e.id)}
-              src={e.image_id}
-              title={e.title}
-              onClick={handleArtworkClick(e.id)}
-              onFavoriteClick={handleFavoriteClick(e.id)}
+  const handlePageClick = useCallback(
+    (i: number) => {
+      void dispatch(
+        fetchArtworksAction({
+          limit: artworkLimitData,
+          page: String(i),
+        }),
+      );
+    },
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [artworkLimitData],
+  );
+
+  return (
+    <section className="w-[90vw] min-h-screen bg-secondary bg-opacity-70 flex justify-center items-center flex-row flex-wrap px-5 mt-4 py-8 rounded-3xl">
+      {status === 'fulfilled' ? (
+        <>
+          <div className="flex flex-row w-full h-auto justify-evenly items-center">
+            <Pagination
+              pageCurrent={pagination?.current_page ?? 1}
+              pageSet={handlePageClick}
+              pageTotal={pagination?.total_pages ?? 1}
+              resultFrom={(pagination?.offset ?? 1) + 1}
+              resultTo={(pagination?.limit ?? 25) * (pagination?.current_page ?? 1)}
+              resultTotal={pagination?.total ?? 1}
             />
-          );
-        },
+            <ListboxMenu />
+          </div>
+          {(data as ArtworkResponseType).map(artwork => {
+            return (
+              <GalleryListItem
+                key={artwork.id}
+                alt={artwork.alt_titles ?? artwork.title}
+                favorite={favoriteArtworks.includes(artwork.id)}
+                src={artwork.image_id}
+                title={artwork.title}
+                onClick={handleArtworkClick(artwork.id)}
+                onFavoriteClick={handleFavoriteClick(artwork.id)}
+              />
+            );
+          })}
+          <div className="flex flex-row w-full h-30 justify-evenly items-center">
+            <Pagination
+              pageCurrent={pagination?.current_page ?? 1}
+              pageSet={handlePageClick}
+              pageTotal={pagination?.total_pages ?? 1}
+              resultFrom={(pagination?.offset ?? 1) + 1}
+              resultTo={(pagination?.limit ?? 25) * (pagination?.current_page ?? 1)}
+              resultTotal={pagination?.total ?? 1}
+            />
+          </div>
+        </>
+      ) : status === 'pending' ? (
+        <p className="text-6xl text-white font-bold">
+          <FormattedMessage defaultMessage="Loading..." id="common:loading" />
+        </p>
+      ) : (
+        <FormattedMessage defaultMessage="Error" id="common:error" />
       )}
-    </section>
-  ) : (
-    <section className="w-full h-full bg-secondary bg-opacity-70 flex justify-center items-center flex-row flex-wrap px-5 py-8 rounded-3xl">
-      {messages.loading.defaultMessage}
     </section>
   );
 };
