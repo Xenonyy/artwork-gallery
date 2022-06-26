9f824d045cab0513948f495842911bd53cf9f9ce
Gonda Armand <gonda.armand@arteries.hu>
2022-06-26 20:59:11 +0200

    feat: update fetch artwork response state

--- src/types/fetchArtworkStateType.ts
+++ src/types/fetchArtworkStateType.ts
@@ -1,9 +1,10 @@
 import type { ArtworkResponseType } from 'types/artworkResponseType';
 import type { PaginationType } from 'types/paginationType';
 import type { PayloadStatusTypes } from 'types/payloadTypes';
+import type { SearchedArtworkType } from 'types/searchedArtworkType';
 
 export type FetchArtworkStateType = {
-  data: ArtworkResponseType;
+  data: ArtworkResponseType | SearchedArtworkType[];
   status: PayloadStatusTypes;
   pagination: PaginationType;
 } | null;
